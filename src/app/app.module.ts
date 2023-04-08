7a86b048f24e96f49368dce32f238613c1896e8d
OleksiiKyrianov <kiral280989@gmail.com>
2023-04-08 12:20:55 +0300

    add markup to right sidebar and main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { LeftSideComponent } from './layouts/left-side/left-side.component';
+import { RigthSideComponent } from './layouts/rigth-side/rigth-side.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    LeftSideComponent,
+    RigthSideComponent
   ],
   imports: [
     BrowserModule,
